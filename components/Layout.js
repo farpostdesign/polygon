779a5b940fcd164040cbce7fd4c8c86122495393
shvetsovdm <sorrows@mail.ru>
2018-09-07 14:05:53 +1000

    Allow Layout children prop. to take array
    now it can take either element or array as a children

--- components/Layout.js
+++ components/Layout.js
@@ -32,7 +32,7 @@ const Layout = ({ children }) => (
 );
 
 Layout.propTypes = {
-    children: PropTypes.element
+    children: PropTypes.oneOfType([ PropTypes.element, PropTypes.array ])
 };
 
 export default Layout;
