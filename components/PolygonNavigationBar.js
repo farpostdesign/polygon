b2f05f1ee1930cfe327a9d31f129e769ff04c3fc
shvetsovdm <sorrows@mail.ru>
2018-09-12 12:22:11 +1000

    Add popover for create button
    to put two buttons under the create button.
    Create project and create design button.

--- components/PolygonNavigationBar.js
+++ components/PolygonNavigationBar.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Button, H1 } from '@blueprintjs/core';
+import { Button, H1, Popover, Menu, MenuItem } from '@blueprintjs/core';
 
 const style = {
     padding: '16px',
@@ -12,7 +12,13 @@ const style = {
 const PolygonNavigationBar = ({ onClickCreateProject }) => (
     <nav style={style}>
         <H1>P</H1>
-        <Button icon="add" large={true} onClick={onClickCreateProject} />
+        <Popover position="bottom-left">
+            <Button icon="add" large={true} />
+            <Menu key="menu">
+                <MenuItem icon="folder-close" text="Add Project" onClick={onClickCreateProject} />
+                <MenuItem icon="media" text="Add Design" />
+            </Menu>
+        </Popover>
     </nav>
 );
 
