35998ff21481a915b90177d8d09975582605b127
shvetsovdm <sorrows@mail.ru>
2018-09-13 11:57:12 +1000

    Move dialog windows from layout to nav. bar
    In order to simplify layout, now it is plain function.

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import PolygonNavigationBar from './PolygonNavigationBar';
-import CreateProject from './CreateProject';
-import CreateDesign from './CreateDesign';
 
 // Global styles
 const style =`
@@ -16,62 +14,27 @@ body, html {
 * { margin: 0; padding:0; box-sizing: border-box; }
 `;
 
-class Layout extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            createProjectIsOpen: false,
-            createDesignIsOpen: false
-        };
-
-        this.handleOpenCreateProject = this.handleOpenCreateProject.bind(this);
-        this.handleCloseCreateProject = this.handleCloseCreateProject.bind(this);
-        this.handleOpenCreateDesign = this.handleOpenCreateDesign.bind(this);
-        this.handleCloseCreateDesign = this.handleCloseCreateDesign.bind(this);
-    }
-
-    render() {
-        return (
-            <div id="layout" style={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100%',
-                width: '100%'
-            }}>
-                <Head>
-                    <link rel="stylesheet" href="/static/normalize.css" />
-                    <link rel="stylesheet" href="/static/blueprintjs/lib/css/blueprint.css" />
-                    <link rel="stylesheet" href="/static/dropzone.min.css" />
-                    <style global>{style}</style>
-                </Head>
-                <div id="container" style={{ display: 'flex', flex: '1 1 auto' }}>
-                    { this.props.navBar && <PolygonNavigationBar onClickCreateProject={this.handleOpenCreateProject} onClickCreateDesign={this.handleOpenCreateDesign} /> }
-                    <div id="content" style={{ flex: '1 1 auto', padding: '16px' }}>
-                        { this.props.children }
-                    </div>
-                </div>
-                <CreateProject isOpen={this.state.createProjectIsOpen} onClose={this.handleCloseCreateProject} />
-                <CreateDesign isOpen={this.state.createDesignIsOpen} onClose={this.handleCloseCreateDesign} />
+const Layout = ({ children, navBar }) => (
+    <div id="layout" style={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100%',
+        width: '100%'
+    }}>
+        <Head>
+            <link rel="stylesheet" href="/static/normalize.css" />
+            <link rel="stylesheet" href="/static/blueprintjs/lib/css/blueprint.css" />
+            <link rel="stylesheet" href="/static/dropzone.min.css" />
+            <style global>{style}</style>
+        </Head>
+        <div id="container" style={{ display: 'flex', flex: '1 1 auto' }}>
+            {navBar && <PolygonNavigationBar />}
+            <div id="content" style={{ flex: '1 1 auto', padding: '16px' }}>
+                {children}
             </div>
-        );
-    }
-
-    handleOpenCreateProject() {
-        this.setState({ createProjectIsOpen: true });
-    }
-
-    handleCloseCreateProject() {
-        this.setState({ createProjectIsOpen: false });
-    }
-
-    handleOpenCreateDesign() {
-        this.setState({ createDesignIsOpen: true });
-    }
-
-    handleCloseCreateDesign() {
-        this.setState({ createDesignIsOpen: false });
-    }
-}
+        </div>
+    </div>
+);
 
 Layout.defaultProps = {
     navBar: true
--- components/PolygonNavigationBar.js
+++ components/PolygonNavigationBar.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, H1, Popover, Menu, MenuItem } from '@blueprintjs/core';
+import CreateProject from './CreateProject';
+import CreateDesign from './CreateDesign';
 
 const style = {
     padding: '16px',
@@ -9,18 +12,53 @@ const style = {
     alignItems: 'center'
 };
 
-const PolygonNavigationBar = ({ onClickCreateProject, onClickCreateDesign }) => (
-    <nav style={style}>
-        <H1>P</H1>
-        <Popover position="bottom-left">
-            <Button icon="add" large={true} />
-            <Menu key="menu">
-                <MenuItem icon="folder-close" text="Add Project" onClick={onClickCreateProject} />
-                <MenuItem icon="media" text="Add Design" onClick={onClickCreateDesign} />
-            </Menu>
-        </Popover>
-    </nav>
-);
+class PolygonNavigationBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            createProjectIsOpen: false,
+            createDesignIsOpen: false
+        };
+
+        this.handleOpenCreateProject = this.handleOpenCreateProject.bind(this);
+        this.handleCloseCreateProject = this.handleCloseCreateProject.bind(this);
+        this.handleOpenCreateDesign = this.handleOpenCreateDesign.bind(this);
+        this.handleCloseCreateDesign = this.handleCloseCreateDesign.bind(this);
+    }
+
+    render() {
+        return (
+            <nav style={style}>
+                <H1>P</H1>
+                <Popover position="bottom-left">
+                    <Button icon="add" large={true} />
+                    <Menu key="menu">
+                        <MenuItem icon="folder-close" text="Add Project" onClick={this.handleOpenCreateProject} />
+                        <MenuItem icon="media" text="Add Design" onClick={this.handleOpenCreateDesign} />
+                    </Menu>
+                </Popover>
+                <CreateProject isOpen={this.state.createProjectIsOpen} onClose={this.handleCloseCreateProject} />
+                <CreateDesign isOpen={this.state.createDesignIsOpen} onClose={this.handleCloseCreateDesign} />
+            </nav>
+        );
+    }
+
+    handleOpenCreateProject() {
+        this.setState({ createProjectIsOpen: true });
+    }
+
+    handleCloseCreateProject() {
+        this.setState({ createProjectIsOpen: false });
+    }
+
+    handleOpenCreateDesign() {
+        this.setState({ createDesignIsOpen: true });
+    }
+
+    handleCloseCreateDesign() {
+        this.setState({ createDesignIsOpen: false });
+    }
+}
 
 PolygonNavigationBar.propTypes = {
     onClickCreateProject: PropTypes.func,
