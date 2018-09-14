882fc65d83098a5e1bfbe08e889f28847572368c
shvetsovdm <sorrows@mail.ru>
2018-09-14 10:28:51 +1000

    Bind menu actions with form components

--- components/BreadcrumbsNav.js
+++ components/BreadcrumbsNav.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { Breadcrumb, Classes, Button } from '@blueprintjs/core';
-import ActionsMenu from './ActionsMenu';
+import { ActionsMenuController } from './ActionsMenu';
 
 const BreadcrumbsNav = ({ items }) => {
     const lastItemIndex = items.length - 1;
@@ -11,9 +11,10 @@ const BreadcrumbsNav = ({ items }) => {
             {ancestors.map((crumb, index) =>
                 <li key={index} className={Classes.BREADCRUMB}><Breadcrumb href={crumb.href} text={crumb.title}/></li>)}
             <li>
-                <ActionsMenu target={
-                    <Button text={currentItem.title} minimal={true} rightIcon="caret-down" large={true} />
-                } />
+                <ActionsMenuController
+                    target={<Button text={currentItem.title} minimal={true} rightIcon="caret-down" large={true} />}
+                    type="project"
+                />
             </li>
         </ul>
     );
--- components/List.js
+++ components/List.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { Icon } from '@blueprintjs/core';
-import ActionsMenu from './ActionsMenu';
+import { ActionsMenuController } from './ActionsMenu';
 
 const listColor = '#7390a2';
 const style = {
@@ -11,7 +11,7 @@ const List = ({ items, icon, actionsMenu }) => {
     const actionTrigger = <Icon icon="more" style={{marginRight: '.75rem', transform: 'rotate(90deg)'}} onClick={this.handleClick} />;
     const ListItem = (item, index) => (
         <li key={item.id || index} style={{ marginBottom: '.75rem' }}>
-            {actionsMenu && <ActionsMenu target={actionTrigger} />}
+            {actionsMenu && <ActionsMenuController target={actionTrigger} type={typeof(item.project) === 'undefined' ? 'project' : 'design'} />}
             <Icon icon={icon} color={listColor} style={{marginRight: '.75rem'}} />
             <a href={item.href}>{item.title}</a>
         </li>
