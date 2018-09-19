202edf6f3d99ca18437475087a7f82e8e38c7434
shvetsovdm <sorrows@mail.ru>
2018-09-20 09:39:52 +1000

    Move AnchorButton style into separate module
    this give us reusability

---
+++ style/index.js
@@ -0,0 +1,34 @@
+const style = {};
+
+const $colorLink = '#6e6eab';
+const borderRadius = '2px';
+
+function border(color) {
+    return `2px solid ${color}`;
+}
+
+style.BUTTON = {
+    display: 'inline-block',
+    padding: '.5rem .7rem',
+    border: border($colorLink),
+    borderRadius,
+    textDecoration: 'none',
+    margin: '0 0 2rem 0',
+    color: $colorLink,
+    fontSize: '1rem'
+};
+
+style.BUTTON_MINIMAL = {
+    border: 'none'
+};
+
+style.INPUT = {
+    padding: '0 10px',
+    height: '2rem',
+    fontSize: 'inherit',
+    lineHeight: 'inherit',
+    border: border('#888'),
+    borderRadius
+};
+
+export default style;
