7947beb78b211d8409e2a8cb3fae96ec8ef85524
shvetsovdm <sorrows@mail.ru>
2018-09-21 16:33:08 +1000

    Add fake "just created" design

--- fakeDesign.js
+++ fakeDesign.js
@@ -1,5 +1,6 @@
 export default [
     { id: 1, title: 'v1', project: 6, href: '/design?id=1' },
     { id: 2, title: 'v2', project: 6, href: '/design?id=2' },
-    { id: 3, title: 'Calendar 2018', project: 1, href: '/design?id=3' }
+    { id: 3, title: 'Calendar 2018', project: 1, href: '/design?id=3' },
+    { id: 100, title: 'Design you have just created', project: 100, href: '/design?id=100' }
 ];
