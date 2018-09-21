13b494a0c32d89420d565198b360854320a691cd
shvetsovdm <sorrows@mail.ru>
2018-09-21 18:00:43 +1000

    Delete unused code
    after 3rd iteration of prototyping

--- components/forms/index.js
+++ components/forms/index.js
@@ -1,12 +1,8 @@
-import ProjectForm from './ProjectForm';
-import DesignForm from './DesignForm';
 import LoginForm from './LoginForm';
 import InlineEdit from './InlineEdit';
 import InlineCreate from './InlineCreate';
 
 export {
-    ProjectForm,
-    DesignForm,
     LoginForm,
     InlineEdit,
     InlineCreate
