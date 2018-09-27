1dbc90419f815e5c16e3c4e91b9d4716fa6bb4da
shvetsovdm <sorrows@mail.ru>
2018-09-27 12:51:33 +1000

    Fix type in H3 component className

--- components/H3.js
+++ components/H3.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-const H3 = ({ id, children }) => <h3 id={id} className=".p-section--header">{children}</h3>;
+const H3 = ({ id, children }) => <h3 id={id} className="p-section--header">{children}</h3>;
 
 H3.propTypes = {
     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
