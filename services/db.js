a063e8d0885da7985e5acc95a10496e2174b430c
shvetsovdm <sorrows@mail.ru>
2018-09-28 16:25:47 +1000

    Add timestamps to DB schemas

--- services/db.js
+++ services/db.js
@@ -12,15 +12,15 @@ mongoose.connect(config.mongooseConnection).catch(errHandler);
 module.exports.ProjectSchema = new Schema({
     name: { type: String, required: true },
     parent: { type: Schema.ObjectId, ref: 'Project' }
-});
+}, { timestamps: true });
 
 module.exports.DesignSchema = new Schema({
     name: { type: String, required: true },
     project: { type: Schema.ObjectId, ref: 'Project', required: true }
-});
+}, { timestamps: true });
 
 module.exports.FileSchema = new Schema({
     name: { type: String, required: true },
     path: { type: String, required: true },
     design: { type: Schema.ObjectId, ref: 'Design', required: true }
-});
+}, { timestamps: true });
