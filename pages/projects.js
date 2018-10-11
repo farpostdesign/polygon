d0add647a1d0b48375b5fe20b6d3d438e46a769f
shvetsovdm <sorrows@mail.ru>
2018-10-11 12:28:07 +1000

    Add create subproject functionality on project page

--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import Router from 'next/router';
 import Layout from '../components/Layout';
 import { InlineCreate } from '../components/forms';
 import List from '../components/List';
@@ -7,6 +8,23 @@ import Section from '../components/Section';
 import H3 from '../components/H3';
 import { InlineEdit } from '../components/forms';
 import 'isomorphic-unfetch';
+
+const store = {
+    dispatch(action) {
+        const type = action.type;
+        return store[type](action);
+    },
+
+    addProject(action) {
+        return fetch('/api/projects', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(action.attributes)
+        }).then((res) => res.json());
+    }
+};
 
 /**
  * Component
@@ -22,7 +40,19 @@ const Projects = ({ project, subProjects, breadcrumbs, designs }) => {
             </Section>
             <Section>
                 <H3>Projects</H3>
-                <InlineCreate name="name" text="Add Subfolder" stubRedirect="/projects?id=100" />
+                <InlineCreate name="name"
+                    text="Add Subfolder"
+                    handleSubmit={(attributes) => {
+                        attributes.parent = project._id;
+                        store.dispatch({ type: 'addProject', attributes })
+                            .then((res) => {
+                                if (res.errors) {
+                                    throw res.errors;
+                                }
+                                const project = res.data;
+                                Router.push(`/projects?id=${project._id}`);
+                            }).catch(alert);
+                    }} />
                 <List icon='folder-close' items={subProjects} />
             </Section>
             <Section>
