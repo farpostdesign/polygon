f4c3fce1e200212054510481cc3d178aa7471878
shvetsovdm <sorrows@mail.ru>
2018-11-27 09:24:47 +1000

    Store now throws more understandable errors

--- services/store.js
+++ services/store.js
@@ -73,12 +73,22 @@ const actions = {
     }
 };
 
+class StoreError extends Error {
+    constructor(msg) {
+        super(msg);
+        this.name = 'Store Error';
+    }
+}
+
 const store = {
     dispatch(action) {
         const type = action.type;
+        if (typeof type === 'undefined') {
+            throw new StoreError('Dispatch function requires property `type` in the argument object');
+        }
         const actionFn = actions[type];
         if (!actionFn) {
-            throw new Error(`No action ${action.type}`);
+            throw new StoreError(`There is no such action as \`${action.type}\``);
         }
         return actionFn(action);
     }
