afa46b71014e3b6ed8245587d33d3a3f2978fe25
shvetsovdm <sorrows@mail.ru>
2018-11-29 17:32:49 +1000

    Add DELETE /api/token
    API to logout

--- api/token.js
+++ api/token.js
@@ -4,6 +4,7 @@ const auth = require('../services/auth');
 const config = require('../config');
 
 const router = express.Router();
+const TOKEN_COOKIE_KEY = 'token';
 
 /**
  * Request for API token
@@ -14,8 +15,16 @@ router.post('/token',
     auth.localMiddleware,
     asyncRoute(async (req, res) => {
         const token = auth.issueToken(req.user);
-        res.cookie('token', token, { secure: config.secureCookie, httpOnly: true });
+        res.cookie(TOKEN_COOKIE_KEY, token, { secure: config.secureCookie, httpOnly: true });
         res.json({ message: 'Authenticated successfully' });
+    })
+);
+
+router.delete('/token',
+    auth.jwtMiddleware,
+    asyncRoute(async (req, res) => {
+        res.clearCookie(TOKEN_COOKIE_KEY);
+        res.json({ message: 'Token removed successfully' });
     })
 );
 
