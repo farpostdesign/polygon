63e675aaee994baecb9749283d9ace3d2453aea8
shvetsovdm <sorrows@mail.ru>
2018-12-04 17:19:15 +1000

    Refactor createuser script

--- bin/createuser.js
+++ bin/createuser.js
@@ -1,11 +1,14 @@
-#!/usr/bin/env node
-
-const email = process.argv[2];
-const password = process.argv[3];
+/**
+ * Sript to add user into database
+ *
+ */
 
 require('../services/db');
 const User = require('../app/models/user');
 const debug = require('debug')('polygon:createuser');
+
+const email = process.argv[2];
+const password = process.argv[3];
 
 const handler = (docOrErr) => {
     debug(docOrErr);
