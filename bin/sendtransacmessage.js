7f577d1e81c6887f1c1d99c5af7c9b41d87db2f9
shvetsovdm <sorrows@mail.ru>
2018-12-17 16:42:56 +1000

    Adapt `npm run sendmessage` to lattest changes

--- bin/sendtransacmessage.js
+++ bin/sendtransacmessage.js
@@ -4,16 +4,16 @@
  *
  */
 
-const { send } = require('../services/transac-message');
+const { send } = require('../services/messaging');
 const debug = require('debug')('polygon:sendtransacmessage');
 
-const DEFAULT_PROVIDER = 'SendGrid';
+const DEFAULT_PROVIDER = 'Mattermost';
 
-const channel = process.argv[3];
-const preferedMessageProvider = process.argv[4] || DEFAULT_PROVIDER;
+const recipient = process.argv[3];
+const messagingProvider = process.argv[4] || DEFAULT_PROVIDER;
 
-if (!channel) {
-    debug('Pass a channel as the first argument to this script');
+if (!recipient) {
+    debug('Pass a recipient as the first argument to this script');
     process.exit(1);
 }
 
@@ -25,9 +25,9 @@ const handler = (docOrErr) => {
     process.exit();
 };
 
-const recipient = {
-    email: channel,
-    preferedMessageProvider
+const user = {
+    messagingProvider,
+    messagingAccount: recipient
 };
 
-send({ to: recipient, message: 'testMessage', name: 'Dear friend' }).then(handler).catch(handler);
+send(user, 'test', { subject: 'This is a test' }).then(handler).catch(handler);
